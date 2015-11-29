52a0693ac59bde816fef0c119f9c32250de1fe88
Rich Tebb <rich@prabang.co.uk>
2015-11-29 12:11:15 +0000

    Rename build tasks for readability

--- build/tasks/build.js
+++ build/tasks/build.js
@@ -7,7 +7,7 @@ var rename = require('gulp-rename');
 var tsd = require('gulp-tsd');
 var ts = require('gulp-typescript');
 var merge = require('merge2');
-var dtsGenerator = require('dts-generator')
+var dtsGenerator = require('dts-generator');
 
 var outputDefFile = paths.output + '/' + paths.pluginName +'.d.ts';
 
@@ -38,48 +38,48 @@ gulp.task('tsd', function (callback) {
   return gulp.src('./gulp_tsd.json').pipe(tsd());
 });
 
-gulp.task('build-html-es6', function () {
+gulp.task('build-html:es6', function () {
   return gulp.src(paths.html)
     .pipe(gulp.dest(paths.output + 'es6'));
 });
 
-gulp.task('build-es6', ['build-html-es6', 'tsd', 'dts-gen'], function () {
+gulp.task('build:es6', ['build-html:es6', 'tsd', 'generate-dts'], function () {
   return compileTsProject('es6', 'commonjs', 'es6');
 });
 
-gulp.task('build-html-commonjs', function () {
+gulp.task('build-html:commonjs', function () {
   return gulp.src(paths.html)
     .pipe(gulp.dest(paths.output + 'commonjs'));
 });
 
-gulp.task('build-commonjs', ['build-html-commonjs', 'tsd', 'dts-gen'], function () {
+gulp.task('build:commonjs', ['build-html:commonjs', 'tsd', 'generate-dts'], function () {
   return compileTsProject('commonjs', 'commonjs', 'es5');
 });
 
-gulp.task('build-html-amd', function () {
+gulp.task('build-html:amd', function () {
   return gulp.src(paths.html)
     .pipe(gulp.dest(paths.output + 'amd'));
 });
 
-gulp.task('build-amd', ['build-html-amd', 'tsd', 'dts-gen'], function () {
+gulp.task('build:amd', ['build-html:amd', 'tsd', 'generate-dts'], function () {
   return compileTsProject('amd', 'amd', 'es5');
 });
 
-gulp.task('build-html-system', function () {
+gulp.task('build-html:system', function () {
   return gulp.src(paths.html)
     .pipe(gulp.dest(paths.output + 'system'));
 });
 
-gulp.task('build-system', ['build-html-system', 'tsd', 'dts-gen'], function () {
+gulp.task('build:system', ['build-html:system', 'tsd', 'generate-dts'], function () {
   return compileTsProject('system', 'system', 'es5');
 });
 
-gulp.task('dts-gen', function(callback) {
+gulp.task('generate-dts', function(callback) {
   dtsGenerator.default({
     name: paths.pluginName,
     project: './src',
     out: outputDefFile,
-    main: 'test-plugin/index'
+    main: paths.pluginName + '/index'
   });
   
   return callback();
@@ -88,7 +88,7 @@ gulp.task('dts-gen', function(callback) {
 gulp.task('build', function(callback) {
   return runSequence(
     ['clean', 'tsd'],
-    ['build-commonjs', 'build-amd', 'build-system'],
+    ['build:commonjs', 'build:amd', 'build:system'],
     callback
   );
 });
